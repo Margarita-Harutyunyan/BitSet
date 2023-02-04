cc78db1ded0f5363ec3275396f697174dc7af7ab
Margarita-Harutyunyan <107138430+Margarita-Harutyunyan@users.noreply.github.com>
2023-02-04 19:19:17 +0400

    Update BitSet.py

--- BitSet.py
+++ BitSet.py
@@ -1,26 +1,26 @@
 class BitVector:
     def __init__(self, size):
-        self.len = size // 32 + 1
-        self.vector = self.len * [0]
+        self.__len = size // 32 + 1
+        self.__vector = self.__len * [0]
 
     def set(self, index):
         block = self.__find_block(index)
         mask = self.__mask_maker(index)
-        self.vector[block] |= mask
+        self.__vector[block] |= mask
 
     def reset(self, index):
         block = self.__find_block(index)
         mask = self.__mask_maker(index)
-        self.vector[block] ^= mask
+        self.__vector[block] ^= mask
 
     def view(self):
-        for block in self.vector:
+        for block in self.__vector:
             print(bin(block), end = ' ')
         print()
         
     def __mask_maker(self, index):
         block = self.__find_block(index)
-        offset = (self.len * 32) - index
+        offset = (self.__len * 32) - index
         mask = 1 << offset
         return mask
     def __find_block(self, index):
